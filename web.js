95afe532c00a06951c8c0e67c9e7206b2d7a678d
Eduard Bardají Puig <ebardaji@gmail.com>
2013-07-24 17:46:43 +0200

    Corrected buffer to string

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var buf=require('buffer');
 var app = express();
 app.use(express.logger());
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString);
-});
+    response.send(fs.readFileSync('index.html').toString());
+    });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+    console.log("Listening on " + port);
+    });
