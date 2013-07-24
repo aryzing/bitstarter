77956dfd3af006a55c509173531b3219d8d91e62
Eduard Bardají Puig <ebardaji@gmail.com>
2013-07-24 17:42:38 +0200

    Contents of website read by web.js from index.html

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var fs=require('fs');
+var buf=require('buffer');
+
 var app = express();
 app.use(express.logger());
-
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync('index.html').toString);
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
