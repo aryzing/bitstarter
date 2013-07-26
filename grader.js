1d42ab5ad0cab30c43820bd3d6af480767acc618
Eduard Bardají Puig <ebardaji@gmail.com>
2013-07-26 03:32:42 +0200

    Grader can now handle urls, backup of original grader

--- grader.js
+++ grader.js
@@ -1,34 +1,15 @@
 #!/usr/bin/env node
-/*
-   Automatically grade files for the presence of specified HTML tags/attributes.
-   Uses commander.js and cheerio. Teaches command line application development
-   and basic DOM parsing.
-
-   References:
-
-   + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
-   + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
-   + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
- */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
+var sys = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 var URL_DEFAULT = "http://calm-brushlands-3751.herokuapp.com/";
 
+//Common functions for both files and urls
 var assertFileExists = function(infile) {
   var instr = infile.toString();
   if(!fs.existsSync(instr)) {
@@ -37,53 +18,71 @@ var assertFileExists = function(infile) {
   }
   return instr;
 };
+var loadChecks = function(checksfile) {
+  return JSON.parse(fs.readFileSync(checksfile));
+};
+//Functions for urls
+function treatresult(url,checksfile){
+  return function(result){
+    //console.log("Web retrieved");
+    var $=cheerio.load(result);
+    var checks = loadChecks(checksfile).sort();
+    var checkJson = {};
+    for(var ii in checks) {
+      var present = $(checks[ii]).length > 0;
+      checkJson[checks[ii]] = present;
+    }
+    var outJson=JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  }
+}
+var checkHtmlUrl= function(url, checksfile) {
+  //console.log(url);
+  rest.get(url).on('complete', treatresult(url, checksfile));
+};
 
-var cheerioHtmlFile = function(htmlfile) {
+//functions for files
+cheerioHtmlFile = function(htmlfile) {
   return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var cheerioHtmlUrl = function(htmlurl) {
-  return cheerio.load(rest.get(htmlurl)
-      .on('complete', function(result){
-        if (result instanceof Error){
-          process.exit(1);
-        }
-        else{
-          return result
-        }
-      }));
-}
-
-      var loadChecks = function(checksfile) {
-        return JSON.parse(fs.readFileSync(checksfile));
-      };
-
-      var checkHtmlFile = function(htmlfile, checksfile) {
-        var $ = cheerioHtmlFile(htmlfile);
-        var checks = loadChecks(checksfile).sort();
-        var out = {};
-        for(var ii in checks) {
-          var present = $(checks[ii]).length > 0;
-          out[checks[ii]] = present;
-        }
-        return out;
-      };
-
+var checkHtmlFile = function(htmlfile, checksfile) {
+  $ = cheerioHtmlFile(htmlfile);
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for(var ii in checks) {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+  }
+  return out;
+};
+//auxiliary function
 var clone = function(fn) {
   // Workaround for commander.js issue.
   // http://stackoverflow.com/a/6772648
   return fn.bind({});
 };
 
-if(require.main == module) {
+if(require.main === module) {
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-    .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-    //.option('-u, --url <url>', 'Public url to analyze', restler, URL_DEFAULT)
+    .option('-u, --url [url]','url of website to analyze')
+    .option('-f, --file [filename]','filename of file to analyze')
     .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
-} else {
+
+  if(program.url){
+    checkHtmlUrl(program.url, program.checks); 
+  }
+  else if(program.file){
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  }
+  else{
+    console.log("Please provide either a -u [URL] OR -f [filename]");
+  }
+}
+else {
   exports.checkHtmlFile = checkHtmlFile;
+  exports.checkHtmlUrl = checkHtmlUrl;
 }
