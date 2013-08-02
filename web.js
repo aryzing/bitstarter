5196308318cee40e3aa5b2734a40b46f8326c6c6
Eduard Bardají Puig <ebardaji@gmail.com>
2013-08-02 21:22:33 +0200

    Trying out fetching local files

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var fs=require('fs');
 var buf=require('buffer');
+var path=require('path');
 
 var app = express();
 app.use(express.logger());
+app.use(express.static(path.join(__dirname, 'bootstrap'));
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index2.html').toString());
+    response.send(fs.readFileSync('index.html').toString());
     });
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
