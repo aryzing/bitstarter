4efb9b4f956f6aa68cc037d451ec0437763e691e
Eduard Bardají Puig <ebardaji@gmail.com>
2013-08-03 11:47:15 +0200

    Trying to read files locally

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var path=require('path');
 
 var app = express();
 app.use(express.logger());
-app.use(express.static(path.join(__dirname, 'bootstrap'));
+app.use(express.static(__dirname, 'bootstrap'));
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index.html').toString());
+    response.send(fs.readFileSync('index.html').toString()));
     });
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
